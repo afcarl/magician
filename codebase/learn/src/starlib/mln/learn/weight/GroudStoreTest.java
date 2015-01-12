6abbbb45a260191c3941845abe6404b64c9126aa
Tuan Anh Pham <tuanh118@gmail.com>
2015-01-12 10:48:42 -0600

    GroundStore test 2

--- codebase/learn/src/starlib/mln/learn/weight/GroudStoreTest.java
+++ codebase/learn/src/starlib/mln/learn/weight/GroudStoreTest.java
@@ -3,7 +3,6 @@ package starlib.mln.learn.weight;
 import java.io.FileNotFoundException;
 
 import starlib.mln.core.MLN;
-import starlib.mln.core.WClause;
 import starlib.mln.store.GroundStore;
 import starlib.mln.store.GroundStoreFactory;
 
@@ -12,15 +11,16 @@ public class GroudStoreTest {
 	
 	public static void main(String[] args) throws FileNotFoundException {
 		// Input files
-//		String mln_file = "test.mln";
-//		String db_file = "test.db";
+		String mln_file = "test.mln";
+		String db_file = "test.db";
 
-		String mln_file = "love_mln.txt";
-		String db_file = "love_mln_db.txt";
+//		String mln_file = "love_mln.txt";
+//		String db_file = "love_mln_db.txt";
 		
 		// GroundStore creation
 		GroundStore gs = GroundStoreFactory.createGraphModBasedGroundStore(mln_file, db_file);
-	
+		gs.update();
+		
 		// Count # true groundings
 		System.out.println("Numbers of true groundings");
 		MLN mln = gs.getMln();
@@ -28,5 +28,9 @@ public class GroudStoreTest {
 			mln.clauses.get(clause_id).print();
 			System.out.printf("Clause %d: %.1f\n\n", clause_id, gs.noOfTrueGroundings(clause_id));
 		}
+		
+		// Flip a ground atom then count
+//		gs.flipAtom(symbol, atomId);
+//		gs.update();
 	}
 }
