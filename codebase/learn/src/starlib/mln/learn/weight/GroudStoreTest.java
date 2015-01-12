f66744615c7d833e40d644ddbf7502fcabd0b86f
Tuan Anh Pham <tuanh118@gmail.com>
2015-01-12 17:49:19 -0600

    Counting test
    Tested noOfTrueGroundings, Implemented iteration over atom groundings

--- codebase/learn/src/starlib/mln/learn/weight/GroudStoreTest.java
+++ codebase/learn/src/starlib/mln/learn/weight/GroudStoreTest.java
@@ -1,15 +1,53 @@
 package starlib.mln.learn.weight;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 
+import starlib.mln.core.Atom;
 import starlib.mln.core.MLN;
+import starlib.mln.core.Term;
 import starlib.mln.store.GroundStore;
 import starlib.mln.store.GroundStoreFactory;
 
 public class GroudStoreTest {
+	static void backtrackCall(List<Integer> a, int index, int n) {
+		if (index >= n) {
+			System.out.println(a.toString());
+			return;
+		}
+		
+		for (int val = 0; val < 2; val++) {
+			a.add(index, val);
+			backtrackCall(a, index+1, n);
+			a.remove(index);
+		}
+	}
+	
+	static void backtrackTest(int n) {
+		List<Integer> a = new ArrayList<Integer>(n);
+		backtrackCall(a, 0, n);
+	}
+
+	/** Get all combinations of term value in an atom */
+	static void backtrack(List<Integer> term_values, int term_index, List<Term> terms) {
+		if (term_index >= terms.size()) {
+			System.out.println(term_values.toString());
+			return;
+		}
+		
+		for (int val : terms.get(term_index).domain) {
+			term_values.add(term_index, val);
+			backtrack(term_values, term_index+1, terms);
+			term_values.remove(term_index);
+		}
+	}
 	
 	
 	public static void main(String[] args) throws FileNotFoundException {
+		// Backtrack
+//		backtrackTest(3);
+		
 		// Input files
 		String mln_file = "test.mln";
 		String db_file = "test.db";
@@ -29,11 +67,45 @@ public class GroudStoreTest {
 		
 		for (int clause_id = 0; clause_id < mln.clauses.size(); clause_id++) {
 			mln.clauses.get(clause_id).print();
-			System.out.printf("Clause %d: %.1f\n\n", clause_id, gs.noOfTrueGroundings(clause_id));
+			System.out.printf("Clause %d: %.1f\n", clause_id, gs.noOfTrueGroundings(clause_id));
+			
+			System.out.println("Domains");
+			
+			for (Atom atom : mln.clauses.get(clause_id).atoms) {
+				backtrack(new ArrayList<Integer>(atom.terms.size()), 0, atom.terms);
+			}
+			
+			Atom atom = mln.clauses.get(clause_id).atoms.get(0);
+			
+			List<Integer> termConstants = new ArrayList<Integer>();
+			termConstants.add(0);
+			termConstants.add(0);
+			
+			int atomId = gs.getGroundAtomId(atom.symbol, termConstants);
+			
+			gs.flipAtom(atom.symbol, atomId);
+			List<Integer> formula_list = new ArrayList<Integer>();
+			formula_list.add(clause_id);
+			gs.update(formula_list);
+			System.out.printf("Flipped: %.1f\n", gs.noOfTrueGroundings(clause_id));
+			gs.unflipAtom(atom.symbol, atomId);
+			
+			System.out.println();
 		}
 		
 		// Flip a ground atom then count
+		List<Integer> termConstants = new ArrayList<Integer>();
+		termConstants.add(0);
+		termConstants.add(0);
+
+//		// Get the predicate symbol
+//		PredicateSymbol symbol = ...;
+//
+//		// Get atomId
+//		int atomId = gs.getGroundAtomId(symbol, termConstatnts);
+//		
+//		// Use atomId to flip atoms etc.
 //		gs.flipAtom(symbol, atomId);
-//		gs.update();
+//		gs.update(symbol);
 	}
 }
